222c7102f9efe1e35968799d52f635deccc478a5
EileenJuergens <juergens.eileen@gmail.com>
2019-12-01 20:04:10 +0100

    feat: create loading action

--- client/src/Redux/actions.js
+++ client/src/Redux/actions.js
@@ -1,6 +1,13 @@
-export const getPosts = data  => {
+export const getPosts = data => {
   return {
     type: 'GET_POSTS',
+    data
+  };
+};
+
+export const getLoading = data => {
+  return {
+    type: 'GET_LOADING',
     data
   };
 };
