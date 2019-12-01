24c6d90a04fa66da0fe3f2f36686946bc79c8c74
EileenJuergens <juergens.eileen@gmail.com>
2019-12-01 20:08:31 +0100

    feat: implement redux

--- client/src/screens/MainScreen.js
+++ client/src/screens/MainScreen.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
+import { getPostsService } from '../Services/APIclient';
+import { getPosts, getLoading } from '../Redux/actions';
 import List from '../OrganismsComponents/List';
 
-const MainScreen = ()  => {
-  return (
-    <View>
-      <Text style={styles.textStyle}>Test from Main</Text>
-      <List/>
-    </View>
-  );
+const MainScreen = ({ postsList, loadingStatus, dispatchGetPosts, dispatchGetLoading })  => {
+  useEffect(() => {
+    dispatchGetLoading(true)
+    getPostsService()
+      .then(response => {
+        dispatchGetLoading(false)
+        dispatchGetPosts(response.data.children)
+      })
+      .catch(() => {
+        dispatchGetLoading(false)
+      })
+  }, []);
+
+  const displayContent = () => {
+    if (loadingStatus) return <Text>Loading...</Text>
+    return (
+      <View>
+        <Text style={styles.textStyle}>Test from Main</Text>
+        <List postsList={postsList}/>
+      </View>
+    );
+  }
+
+  return displayContent();
 };
 
 const styles = StyleSheet.create({
@@ -17,4 +37,14 @@ const styles = StyleSheet.create({
   }
 });
 
-export default MainScreen;
+const mapStateToProps = state => ({
+  postsList: state.postsReducer,
+  loadingStatus: state.loadingReducer
+});
+
+const mapDispatchToProps = dispatch => ({
+  dispatchGetPosts: data => dispatch(getPosts(data)),
+  dispatchGetLoading: data => dispatch(getLoading(data))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainScreen);
